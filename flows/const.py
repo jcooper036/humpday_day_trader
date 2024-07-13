d7bdd8267dd478ab7a4cd6a2776387fdd33a806b
Jacob Cooper <jcooper036@gmail.com>
2024-07-13 15:32:13 -0600

    report building complete

--- flows/const.py
+++ flows/const.py
@@ -1,1 +1,3 @@
 CURRENT_STOCK_FILE = "flows/data/current_stock.txt"
+CURRENT_MKDOWN_FILE = "flows/data/report.md"
+CURRENT_PLOT_FILE = "flows/data/plot.png"
--- flows/prospecter.py
+++ flows/prospecter.py
@@ -1,9 +1,19 @@
+from datetime import datetime, timedelta
+
+import finnhub
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.ticker import FuncFormatter
 import pandas as pd
 from prefect import flow, task
+import seaborn as sns
 
-from flows.const import CURRENT_STOCK_FILE
+from flows import env
+from flows import const
 
-CURRENT_STOCK_FILE = ""
+# non-interactive backend as to not crash the thread
+matplotlib.use("agg")
 
 
 def get_wikipedia_nasdaq100() -> pd.DataFrame:
@@ -16,24 +26,134 @@ def get_wikipedia_nasdaq100() -> pd.DataFrame:
 @task
 def pick_random_stock() -> dict:
     nasdaq_100 = get_wikipedia_nasdaq100()
-    return nasdaq_100.sample(n=1).to_dict("records")
+    return nasdaq_100.sample(n=1).to_dict("records")[0]
 
 
 @task
 def save_current_stock(stock: dict):
-    with open(CURRENT_STOCK_FILE, "w") as f:
+    with open(const.CURRENT_STOCK_FILE, "w+") as f:
         f.write(stock["Ticker"])
+
+
+@task
+def get_stock_data(stock: dict) -> dict:
+    symbol: str = stock["Ticker"]
+    today: str = datetime.now().date().strftime("%Y-%m-%d")
+    one_year_ago: str = (datetime.now().date() - timedelta(days=3 * 365)).strftime(
+        "%Y-%m-%d"
+    )
+    data = {"symbol": symbol}
+    data.update(stock)
+    data = {k.lower().replace(" ", "_"): v for k, v in data.items()}
+
+    # use finnhub to collect some data
+    # limit 60 API calls / minute
+    finnhub_client = finnhub.Client(api_key=env.FINNHUB_API_KEY)
+    quote = finnhub_client.quote(symbol=symbol)
+    data["current_price"] = quote["c"]
+    basic_fiancials = finnhub_client.company_basic_financials(symbol, metric="all")
+    data.update({m: basic_fiancials["metric"][m] for m in ["52WeekHigh", "52WeekLow"]})
+    insider_info = finnhub_client.stock_insider_transactions(
+        symbol=symbol,
+        _from=one_year_ago,
+        to=today,
+    )
+    data["insider_transactions"] = insider_info["data"]
+    company_profile = finnhub_client.company_profile2(symbol=symbol)
+    data.update({m: company_profile[m] for m in ["name", "weburl"]})
+    # company_news = finnhub_client.company_news(
+    #     symbol,
+    #     _from=one_year_ago,
+    #     to=today,
+    # )
+    data["recommendation_trends"] = finnhub_client.recommendation_trends(symbol)
+    return data
+
+
+@task
+def build_report(stock_data):
+
+    # mkdown data
+    sd = stock_data
+    mkdown_data = f"""## {sd['company']} ({sd['ticker']}) | {sd['weburl']}
+GICS Sector:{sd["gics_sector"]}\tGICS Sub-Industry:{sd["gics_sub-industry"]} 
+**cuurent price:{sd["current_price"]}** 52WeekHigh:{sd["52WeekHigh"]} 52WeekLow:{sd["52WeekLow"]}
+    """
+    print(mkdown_data)
+    with open(const.CURRENT_MKDOWN_FILE, "w+") as f:
+        f.write(mkdown_data)
+
+    # plot
+    # format the insider data
+    df = pd.DataFrame(stock_data["insider_transactions"])
+    # chrome-extension://oemmndcbldboiebfnladdacbdfmadadm/https://www.sec.gov/about/forms/form4data.pdf
+    # code S is the sale of private assets. There are others that might apply, but this
+    # is a simple one
+    df = df.query("transactionCode=='S'").reset_index(drop=True)
+
+    df.transactionDate = pd.to_datetime(df.transactionDate)
+    df = df.sort_values("transactionDate", ascending=True)
+    # we drop a bit of data, but we expect these values to be very similar so it's fine
+    df = df.drop_duplicates(subset="transactionDate")
+    df = df.set_index("transactionDate")
+
+    rule = 30
+    sparse = df.index.to_series().diff().dt.days.ge(rule)
+    new_timestamps = df.index[sparse] - pd.Timedelta(days=rule)
+    df_ = df.reindex(df.index.union(new_timestamps))
+    df_["month"] = df_.index.astype(str).str[0:7]
+    month_ma = df_.groupby("month").transactionPrice.mean().interpolate()
+    month_ma.name = "month_moving_average"
+    df_ = df_.merge(month_ma, on="month")
+    insider_price_month_average = (
+        df_[["month", "month_moving_average"]].drop_duplicates().reset_index(drop=True)
+    )
+
+    # add in the analyst data
+    ar_data = pd.DataFrame(stock_data["recommendation_trends"])
+    ar_data["month"] = ar_data["period"].str[0:7]
+    pdf = insider_price_month_average.merge(ar_data, on="month", how="outer")
+
+    # build a plot and save it
+    _, axes = plt.subplots(2, 1, figsize=(20, 10))
+    sns.lineplot(
+        pdf,
+        x="month",
+        y="month_moving_average",
+        ax=axes[0],
+    )
+    axes[0].tick_params(axis="x", rotation=45)
+    axes[0].set_title("Insider sell price")
+
+    patches = [
+        ("strongBuy", "darkblue"),
+        ("buy", "lightblue"),
+        ("hold", "orange"),
+        ("sell", "red"),
+        ("strongSell", "darkred"),
+    ]
+    legend_handles = []
+    for c, col in patches:
+        sns.barplot(x="month", y=c, data=pdf, color=col, ax=axes[1])
+        legend_handles.append(mpatches.Patch(color=col, label=c))
+
+    plt.legend(handles=legend_handles)
+
+    axes[1].tick_params(axis="x", rotation=45)
+    axes[1].set_title("Analyst reccomendations")
+    axes[1].yaxis.set_major_formatter(FuncFormatter(lambda x, _: int(x)))
+    plt.tight_layout()
+    plt.savefig(const.CURRENT_PLOT_FILE)
 
 
 @flow(log_prints=True)
 def prospector():
     stock = pick_random_stock()
-
-    print("hello there")
-    # pick a random stock
-    # write that stock to a local file called "next stock"
-    # put together the prospectus
-    # what is this company?
-    # how has their stock performed over the last week?
-    # including the performance from 10-1030 on wednesdys
+    save_current_stock(stock=stock)
+    stock_data = get_stock_data(stock)
+    build_report(stock_data=stock_data)
     # send this to a slack channel
+
+
+if __name__ == "__main__":
+    prospector()
