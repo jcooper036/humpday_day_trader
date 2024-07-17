9866abf929f3e7c29cf4d60852ded2b826fd4c2b
Jacob Cooper <jcooper036@gmail.com>
2024-07-16 22:47:09 -0600

    add reporter

--- flows/reporter.py
+++ flows/reporter.py
@@ -1,35 +1,81 @@
-import enum
-
-from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
-from prefect import flow
+from alpaca.trading.models import Order
+from prefect import flow, task
+from prefect_slack import SlackCredentials
+from prefect_slack.messages import send_chat_message
 
 from flows.alpaca_client import AccountType, get_client
 import flows.const as const
-import flows.env as env
+from flows.trader import get_market_price
 
 
-@flow(log_prints=True)
-def reporter(account_type: AccountType = AccountType.PAPER, slack_channel=const.CHANNELS['']):
-    # get the symbol
-    with open(const.CURRENT_STOCK_FILE, "r") as f:
-        ticker = f.readlines()[0]
-    print(ticker)
-    
-    # sell the postion
+@task
+def post_to_slack(order: Order, market_price: float, slack_channel: const.SlackChannel):
+    blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "plain_text",
+                "text": ":moneybag: Postion sold",
+            },
+        },
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": f"{order.symbol}",
+            },
+        },
+        {"type": "divider"},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"qty: {order.qty}\nprice: {market_price}\ntotal value: {round((float(order.qty) * market_price), 2)}\n:chart_with_upwards_trend:",
+            },
+        },
+    ]
+    slack_credentials = SlackCredentials.load("slackbot-cred")
+    send_chat_message(
+        slack_credentials=slack_credentials,
+        slack_blocks=blocks,
+        channel=slack_channel.name,
+    )
+
+
+@task
+def sell_position(account_type: AccountType, ticker):
     trading_client = get_client(account_type=account_type)
     postions = trading_client.get_all_positions()
-    pos = [p for p in postions if p["symbol"] == ticker][0]
+    pos = [p for p in postions if p.symbol == ticker][0]
 
     market_order_data = MarketOrderRequest(
         symbol=ticker,
-        qty=pos["qty"],
+        qty=pos.qty,
         side=OrderSide.SELL,
         time_in_force=TimeInForce.DAY,
     )
     market_order = trading_client.submit_order(order_data=market_order_data)
-    
+    return market_order
+
+
+@flow(log_prints=True)
+def reporter(
+    account_type: AccountType = AccountType.PAPER,
+    slack_channel: const.SlackChannel = const.CHANNELS["test"],
+):
+    # get the symbol
+    with open(const.CURRENT_STOCK_FILE, "r") as f:
+        ticker = f.readlines()[0]
+    print(ticker)
+
+    sell_order = sell_position(account_type=account_type, ticker=ticker)
+    market_price = get_market_price(ticker=ticker)
+    post_to_slack(
+        order=sell_order, market_price=market_price, slack_channel=slack_channel
+    )
+
     # read the stock name for the file
     # read the parquet information, make sure the two match
     # send request to the alpaca API to confirm that the postion has been exited
@@ -37,3 +83,7 @@ def reporter(account_type: AccountType = AccountType.PAPER, slack_channel=const.
     # write the stock information to the parquet
     # generate a report on the performance, adding to the prospectors report
     # send to slack
+
+
+if __name__ == "__main__":
+    reporter()
