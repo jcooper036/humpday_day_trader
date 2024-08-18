df85b5fdcae3d7623850aef5f52fd4dab9a158d8
Jacob Cooper <jcooper036@gmail.com>
2024-08-18 08:50:07 -0600

    etf env bugfix

--- flows/etf_trading/etf_specs.py
+++ flows/etf_trading/etf_specs.py
@@ -1,4 +1,4 @@
 import os
 
-etf_options = os.environ.get("ETF_PICKS").split(",")
+etf_options = os.environ.get("ETF_PICKS")
 etf_options = etf_options.split(",") if isinstance(etf_options, str) else []
